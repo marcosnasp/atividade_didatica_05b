2a6f21c4d2caed7198fb9889f09c62b11bfc78cd
Marcos Portela <marcosnasp@gmail.com>
2021-09-02 08:58:35 -0300

    Remoção de arquivos para organização do repositório
    Removendo para fins de organização

--- funcoes.hpp
+++
@@ -1,7 +0,0 @@
-#ifndef FUNCOES_HPP_INCLUDED
-#define FUNCOES_HPP_INCLUDED
-void vandermonde(float *lambda, short N, float *A);
-int retorna_param(int n);
-void criar_matriz(int linhas, int colunas, float *matriz_criada);
-void imprimir_matriz(int linhas, int colunas, float* matriz);
-#endif // FUNCOES_HPP_INCLUDED
